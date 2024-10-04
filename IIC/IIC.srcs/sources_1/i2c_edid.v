d63bb9557a202fe36d25b0c087a559a79607a151
MoonGrt <1561145394@qq.com>
2024-10-04 14:18:41 +0800

    modified:   IIC/IIC.srcs/sources_1/i2c_edid.v 	new file:   SPI/SPI.srcs/sources_1/spi.v

--- IIC/IIC.srcs/sources_1/i2c_edid.v
+++ IIC/IIC.srcs/sources_1/i2c_edid.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+asduhahsdasidohiahdb`timescale 1ns / 1ps
 //****************************************Copyright (c)***********************************//
 //原子哥在线教学平台：www.yuanzige.com
 //抿术支持：www.openedv.com
